42beba9b7051625e627084d1db1038204107218c
Ana-Agudelo10 <valeagudelob10@gmail.com>
2024-05-06 23:51:58 -0500

    Second Class - Java

--- src/App.java
+++ src/App.java
@@ -1,41 +1,29 @@
-import java.util.Scanner;
+import modelo.Pelicula;
+import modelo.Serie;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Pelicula Matrix");
-        //Declaración de variables:
-        int fechaDeLanzamiento = 1999;
-        double evaluacion = 4.5;
-        boolean incluidoEnElPlanBasico = true;
-        String nombre = "Matriz";
-        String sinopsis = """
-                La mejor pelicula del fin del milenio
-                """;
+        Principal principal = new Principal();
+        principal.muestraElMenu();
+        //Se crea un nuevo objeto de la pelicula
+        // Pelicula matrix = new Pelicula();
+        // matrix.setNombre("Matrix");
+        // matrix.setTiempoDeDuracionEnMinutos(120);
+        // matrix.setFechaDeLanzamiento(1999);
+        // matrix.muestraFichaTecnica();
 
-        double mediaEvaluacionesUsuario = 0;
+        // Pelicula encanto = new Pelicula();
+        // encanto.setNombre("Encanto");
+        // encanto.setTiempoDeDuracionEnMinutos(120);
+        // encanto.setFechaDeLanzamiento(2022);
+        // encanto.muestraFichaTecnica();
 
-        System.out.println("Pelicula: " + nombre);
-        System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
-        System.out.println("Evaluación: " + evaluacion);
-        System.out.println("Incluido en el plan basico: " + incluidoEnElPlanBasico);
-        System.out.println("Sinopsis: " + sinopsis);
-
-        double mediaEvaluacion = (4.5 + 4.8 + 3) / 3;
-        System.out.println("Media de la evaluación de Matrix: " + mediaEvaluacion);
-
-        if (fechaDeLanzamiento < 2023){
-            System.out.println("Pelicula Popular en el momento");
-        } else {
-            System.out.println("Pelicula Retro que vale la pena ver");
-        }
-
-        for (int i = 0; i < 3; i++) {
-            Scanner teclado = new Scanner(System.in);
-            System.out.println("Ingresa la evaluación que le darias a Matrix: ");
-            double notaMatrix = teclado.nextDouble();
-            mediaEvaluacionesUsuario = mediaEvaluacionesUsuario + notaMatrix;
-        }
-        System.out.println("La media de la pelicula Matrix calculada por el usuario es: " + mediaEvaluacionesUsuario/3);
+        // Serie friends = new Serie();
+        // friends.setNombre("Friends");
+        // friends.setFechaDeLanzamiento(1996);
+        // friends.setTemporadas(10);
+        // friends.setEpisodiosPorTemporada(22);
+        // friends.setDuracionEnMinutosPorEpisodio(30);
+        // friends.muestraFichaTecnica();
     }
 }
---
+++ src/modelo/Titulo.java
@@ -0,0 +1,80 @@
+package modelo;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Titulo {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private double evaluacion;
+    private boolean incluidoEnElPlanBasico;
+    private String sinopsis;
+    private int tiempoDeDuracionEnMinutos;
+
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+
+
+    public boolean isIncluidoEnElPlanBasico() {
+        return incluidoEnElPlanBasico;
+    }
+
+
+
+    public void setIncluidoEnElPlanBasico(boolean incluidoEnElPlanBasico) {
+        this.incluidoEnElPlanBasico = incluidoEnElPlanBasico;
+    }
+
+
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+
+
+    public int getTiempoDeDuracionEnMinutos() {
+        return tiempoDeDuracionEnMinutos;
+    }
+
+
+
+    public void setTiempoDeDuracionEnMinutos(int tiempoDeDuracionEnMinutos) {
+        this.tiempoDeDuracionEnMinutos = tiempoDeDuracionEnMinutos;
+    }
+
+
+
+    public void muestraFichaTecnica(){
+        System.out.println("****FICHA TÉCNICA****");
+        System.out.println("Nombre del titulo: " + nombre);
+        System.out.println("Fecha de Lanzamiento: " + fechaDeLanzamiento);
+        System.out.println("Tiempo de duración: " + getTiempoDeDuracionEnMinutos() + "minutos");
+    }
+}
